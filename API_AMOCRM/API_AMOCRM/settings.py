31156567b2119ab13318d6972f86782facc6ba2b
Ghost <yasinabdurakhmanov@gmail.com>
2021-11-16 20:16:21 +0300

    changes

--- API_AMOCRM/API_AMOCRM/settings.py
+++ API_AMOCRM/API_AMOCRM/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qu2z)lo54o1mo$2sb48ri0y1oi9z96u8pw#=k2%n!qt660(!5^'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
